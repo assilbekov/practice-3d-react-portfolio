bbc6b98eb566051695a1d55dde98fc07260efad9
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 09:58:29 -0300

    Sky added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,17 +2,18 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Loader } from "../components/Loader";
 import { Island } from "../models/island";
+import { Sky } from "../models/Sky";
 
 export const Home = () => {
-  const adjustIslandForScreenSize  = () => {
+  const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43]
     let rotation = [0.1, 4.7, 0]
 
-    if(window.innerWidth < 768) {
+    if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9]
     } else {
-      screenScale = [1, 1, 1] 
+      screenScale = [1, 1, 1]
     }
 
     return [screenScale, screenPosition, rotation]
@@ -29,12 +30,11 @@ export const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
+          <Sky />
           <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
         </Suspense>
       </Canvas>
