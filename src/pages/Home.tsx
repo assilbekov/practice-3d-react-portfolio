c0a88e568ac866e1494bdd3006a21af25acf6ffe
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 07:41:36 -0300

    Home added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+
+export const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <h1 className="text-4xl font-bold">Home</h1>
+      <p className="text-lg">Welcome to my portfolio</p>
+    </div>
+  )
+}
