ae0fadd088a042a66838625778283a729e921fe5
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 07:21:31 -0300

    add Navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom"
+
+
+export const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+        <p className="blue-gradient-text">AA</p>
+      </NavLink>
+    </header>
+  )
+}
