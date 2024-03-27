850dec75c3b84751941d83f06936539e1a74d4cd
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 08:26:25 -0300

    Loader added

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+
+
+export const Loader = () => {
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 z-50 flex items-center justify-center bg-slate-300/50">
+      <div className="w-24 h-24 border-4 border-slate-300 rounded-full animate-spin"></div>
+    </div>
+  )
+}
