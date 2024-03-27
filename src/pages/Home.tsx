b4a5533df56056d25781f6183b4f5e50ddaa2550
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 09:49:26 -0300

    island preview added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Loader } from "../components/Loader";
+import { Island } from "../models/island";
 
 export const Home = () => {
+  const adjustIslandForScreenSize  = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0]
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9]
+    } else {
+      screenScale = [1, 1, 1] 
+    }
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
   return (
-    <section className="w-full h-full relative">
+    <section className="w-full h-screen relative">
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
       </div> */}
@@ -18,6 +34,8 @@ export const Home = () => {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+
+          <Island position={islandPosition} scale={islandScale} rotation={islandRotation} />
         </Suspense>
       </Canvas>
     </section>
