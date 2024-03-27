e1fa5d5a75ccf69198c2b87262610b9ac77542c1
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 07:44:10 -0300

    import pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import { Navbar } from "./components/navbar"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Projects } from "./pages/Projects"
+import { Contact } from "./pages/Contact"
 
 
 function App() {
@@ -8,10 +12,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={"Home"} />
-          <Route path="/about" element={"About"} />
-          <Route path="/projects" element={"Projects"} />
-          <Route path="/contact" element={"Contact"} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
