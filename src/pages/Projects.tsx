0acafb5bac22e0cfe00514325996a823cb87792b
assilbekov <askhat.assilbekov@gmail.com>
2024-03-27 07:43:18 -0300

    Projects added

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,9 @@
+
+
+export const Projects = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5">
+      <h1 className="text-4xl font-bold">Projects</h1>
+    </div>
+  )
+}
