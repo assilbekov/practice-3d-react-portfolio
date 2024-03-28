fdd1e248384f3501d2f3129f9be39732a971f78e
assilbekov <askhat.assilbekov@gmail.com>
2024-03-28 03:43:18 -0300

    Contact form added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
 
 
 export const Contact = () => {
+  const [form, setForm] = useState({ name: "", email: "", message: "" })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center gap-5">
-      <h1 className="text-4xl font-bold">Contact</h1>
-    </div>
+    <section className="flex relative flex-col lg:flex-row max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form className="flex w-full flex-col gap-7 mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              value={form.name}
+              onChange={handleChange}
+            />
+          </label>
+        </form>
+      </div>
+    </section>
   )
 }
