6f6662bfbd5eb7cb014f23c70378c594480bf239
assilbekov <askhat.assilbekov@gmail.com>
2024-03-28 03:32:28 -0300

    homeinfo second stage added

--- src/components/HomeInfo.tsx
+++ src/components/HomeInfo.tsx
@@ -1,4 +1,15 @@
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons"
 
+const InfoBox = ({ text, link, btnText }) => (
+  <div className="info-box">
+    <p className="font-medium sm:text-xl text-center">{text}</p>
+    <Link to={link} className="neo-brutalism-white neo-btn">
+      {btnText}
+      <img src={arrow} className="w-4 h-4 object-contain" />
+    </Link>
+  </div>
+)
 
 const renderContent = {
   1: (
@@ -7,19 +18,15 @@ const renderContent = {
     </h1>
   ),
   2: (
-    <h1>2</h1>
+    <InfoBox text="Worked with many companies and picked up many skills" link="/about" btnText="Learn more" />
   ),
   3: (
-    <h1>4</h1>
+    <h1>3</h1>
   ),
   4: (
     <h1>4</h1>
   ),
 }
-
-const InfoBox = ({ text, link, btnText }) => (
-  <div></div>
-)
 
 export const HomeInfo = ({ currentStage }) => {
   return renderContent[currentStage] ?? null;
